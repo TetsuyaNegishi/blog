cb3e4929a1da859cc1e7e8d9159d4052c07faf70
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2019-07-03 00:44:39 +0900

    chore: pages配下をtsx化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { Link, graphql, PageRendererProps } from "gatsby"
 
 // import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
-class BlogIndex extends React.Component {
+class BlogIndex extends React.Component<Props> {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
@@ -41,6 +41,12 @@ class BlogIndex extends React.Component {
     )
   }
 }
+
+interface QueryProps {
+  data: any;
+}
+
+type Props = QueryProps & PageRendererProps;
 
 export default BlogIndex
 
