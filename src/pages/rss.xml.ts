a36e6db55861f0b145e245abcfb4bca673d3f482
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2023-04-23 11:20:49 +0900

    feat: Apply eslint to .astro and .ts files

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,60 +1,60 @@
-import rss from "@astrojs/rss";
-import { blog } from "../lib/markdoc/frontmatter.schema";
-import { read } from "../lib/markdoc/read";
-import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from "../config";
-import { globby } from "globby";
+import rss from '@astrojs/rss'
+import { blog } from '../lib/markdoc/frontmatter.schema'
+import { read } from '../lib/markdoc/read'
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from '../config'
+import { globby } from 'globby'
 
 export const get = async () => {
-  const paths = await globby(`content/blog/*/*.md`);
+  const paths = await globby('content/blog/*/*.md')
   const posts = await Promise.all(paths.map(async path => {
-    const post = await read({filepath: path, schema: blog})
+    const post = await read({ filepath: path, schema: blog })
     const year = path.split('/')[2]
-    return {...post, year}
+    return { ...post, year }
   }))
 
   const sortedPosts = posts
-    .filter((p) => p.frontmatter.draft !== true)
+    .filter((p) => !p.frontmatter.draft)
     .sort(
       (a, b) =>
         new Date(b.frontmatter.date).valueOf() -
         new Date(a.frontmatter.date).valueOf()
-    );
+    )
 
-  let baseUrl = SITE_URL;
+  let baseUrl = SITE_URL
   // removing trailing slash if found
   // https://example.com/ => https://example.com
-  baseUrl = baseUrl.replace(/\/+$/g, "");
+  baseUrl = baseUrl.replace(/\/+$/g, '')
 
   const rssItems = sortedPosts.map(({ frontmatter, slug, year }) => {
     if (frontmatter.external) {
-      const title = frontmatter.title;
-      const pubDate = frontmatter.date;
-      const link = frontmatter.url;
+      const title = frontmatter.title
+      const pubDate = frontmatter.date
+      const link = frontmatter.url
 
       return {
         title,
         pubDate,
-        link,
-      };
+        link
+      }
     }
 
-    const title = frontmatter.title;
-    const pubDate = frontmatter.date;
-    const description = frontmatter.description;
-    const link = `${baseUrl}/${year}/${slug}`;
+    const title = frontmatter.title
+    const pubDate = frontmatter.date
+    const description = frontmatter.description
+    const link = `${baseUrl}/${year}/${slug}`
 
     return {
       title,
       pubDate,
       description,
-      link,
-    };
-  });
+      link
+    }
+  })
 
-  return rss({
+  return await rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: baseUrl,
-    items: rssItems,
-  });
-};
+    items: rssItems
+  })
+}
