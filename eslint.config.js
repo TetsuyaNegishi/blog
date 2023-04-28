910ddb8a4f3941d120bae7b40641e97eac82d50c
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2023-04-29 08:32:16 +0900

    feat: add astro and ts config to eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,29 @@
 const { FlatCompat } = require('@eslint/eslintrc')
+const ts = require('@typescript-eslint/eslint-plugin')
+const tsParser = require('@typescript-eslint/parser')
 
 const compat = new FlatCompat()
+
+const jsConfig = compat.extends('eslint-config-standard')[2]
+jsConfig.files = ['**/*.js', '**/*.mjs', '**/*.cjs']
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
 module.exports = [
   {
     ignores: ['dist']
   },
-  ...compat.extends('eslint-config-standard')
+  ...compat.extends('plugin:astro/recommended'),
+  jsConfig,
+  {
+    files: ['src/*.ts'],
+    languageOptions: {
+      parser: tsParser
+    },
+    plugins: {
+      '@typescript-eslint': ts
+    },
+    rules: {
+      ...ts.configs.recommended.rules
+    }
+  }
 ]
